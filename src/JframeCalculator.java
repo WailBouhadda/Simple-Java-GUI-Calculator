bc45ebb2438e9b12267638e287d2a45e7a13f29c
WAIL-20 <wailbouhadda1@gmail.com>
2020-04-01 12:39:23 +0200

    Add ActionListener

--- src/JframeCalculator.java
+++ src/JframeCalculator.java
@@ -1,8 +1,13 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class JframeCalculator {
+
+    
+
     private JPanel JCalculator;
-    private JTextField textField1;
+    private JTextField TextDisplay;
     private JButton button1;
     private JButton button2;
     private JButton button3;
@@ -23,6 +28,86 @@ public class JframeCalculator {
     private JButton BTNMinus;
     private JButton BTNMultiply;
     private JButton BTNDivide;
+
+    public JframeCalculator() {
+        BTN1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String BTN1Text = TextDisplay.getText() + BTN1.getText();
+                TextDisplay.setText(BTN1Text);
+            }
+        });
+        BTN2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String BTN2Text = TextDisplay.getText() + BTN2.getText();
+                TextDisplay.setText(BTN2Text);
+            }
+        });
+        BTN3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String BTN3Text = TextDisplay.getText() + BTN3.getText();
+                TextDisplay.setText(BTN3Text);
+            }
+        });
+        BTN4.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String BTN4Text = TextDisplay.getText() + BTN4.getText();
+                TextDisplay.setText(BTN4Text);
+            }
+        });
+        BTN5.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String BTN5Text = TextDisplay.getText() + BTN5.getText();
+                TextDisplay.setText(BTN5Text);
+            }
+        });
+        BTN6.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String BTN6Text = TextDisplay.getText() + BTN6.getText();
+                TextDisplay.setText(BTN6Text);
+            }
+        });
+        BTN7.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String BTN7Text = TextDisplay.getText() + BTN7.getText();
+                TextDisplay.setText(BTN7Text);
+            }
+        });
+        BTN8.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String BTN8Text = TextDisplay.getText() + BTN8.getText();
+                TextDisplay.setText(BTN8Text);
+            }
+        });
+        BTN9.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String BTN9Text = TextDisplay.getText() + BTN9.getText();
+                TextDisplay.setText(BTN9Text);
+            }
+        });
+        BTN0.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String BTN0Text = TextDisplay.getText() + BTN0.getText();
+                TextDisplay.setText(BTN0Text);
+            }
+        });
+        BTNPoint.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String BTNPointText = TextDisplay.getText() + BTNPoint.getText();
+                TextDisplay.setText(BTNPointText);
+            }
+        });
+    }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("JframeCalculator");
